5f37b6b1fe791a870a547c753f181f12f0c588a9
Guilherme Haddad <guiux.haddad@gmail.com>
2017-05-31 22:40:14 -0300

    Funcionalidade F8

--- src/SuperCalc.java
+++ src/SuperCalc.java
@@ -1,8 +1,19 @@
 
 public class SuperCalc {
 	
+	
+	
+	
 	public int f7 (int x){
 		return x + 1;
+	}
+	
+	
+	public boolean isZero (int x){
+		if(x == 0)
+			return true;
+		else
+			return false;
 	}
 	
 }
